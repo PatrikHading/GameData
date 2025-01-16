85a9c9d33dd4b3d708257ae2a50dc1e8296b1fd8
hading <hading@gmail.com>
2025-01-17 00:07:48 +0100

    Added some tests to see if it works.

---
+++ src/test/java/com/example/GameData/DatabaseConnectionTest.java
@@ -0,0 +1,30 @@
+package com.example.GameData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+@ActiveProfiles("test")
+
+public class DatabaseConnectionTest {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    public void testConnection() throws SQLException {
+        try (Connection connection = dataSource.getConnection()) {
+            assertNotNull(connection, "Connection should not be null");
+            System.out.println("Connection established");
+        }
+    }
+
+}
